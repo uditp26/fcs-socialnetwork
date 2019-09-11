c0ac0e66848ea1ccfb98cfc9abfd3651dc0a6d8a
Udit Pant <udit18049@iiitd.ac.in>
2019-09-11 19:26:31 +0530

    Toolbar added.

--- casual_user/views.py
+++ casual_user/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic import View
 from django.http import Http404
+
 from .models import CasualUser
+
+from django.contrib.auth import logout
+from django.http import HttpResponse, HttpResponseRedirect
 
 class HomepageView(View):
     template_name = 'casual_user/homepage.html'
@@ -17,4 +21,9 @@ class HomepageView(View):
             name = casual_user.user.first_name + ' ' +  casual_user.user.last_name
             bundle = {'name': name, 'date_of_birth': casual_user.date_of_birth, 'gender': casual_user.gender,'phone': casual_user.phone}
             return render(request, self.template_name, {'current_user':bundle})
-            
+
+class LogoutView(View):
+    template_name = 'login/login.html'
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('applogin:login'))
